aebc4f125ed36bc30baed30fa8716c54d82f189c
Riya Chatterjee <56023007+rrriiiyyyaaa@users.noreply.github.com>
2021-11-27 20:49:53 +0530

    user input and conditional statements

---
+++ day2/3.java
@@ -0,0 +1,20 @@
+
+// to print date taking user input.
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the date: ");
+            int date = sc.nextInt();
+            System.out.print("Enter the month: ");
+            int month = sc.nextInt();
+            System.out.print("Enter the year: ");
+            int year = sc.nextInt();
+            System.out.println( "The date is "+date+ "-"+ month + "-"+ year);
+            
+          }
+}
+
+
