799b2235348ae00007400107ce74c5aa7f630772
Riya Chatterjee <56023007+rrriiiyyyaaa@users.noreply.github.com>
2021-11-29 12:06:25 +0530

    Add files via upload

---
+++ day4/1.java
@@ -0,0 +1,30 @@
+/*
+
+*/
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter the year: ");
+	    // Take integer variable year and assign a value to it.
+	    int year = sc.nextInt();
+	    
+	    // 1. century- if it is divisibe by 100 and 400 both, it is a leap year (century).  Ex- 2000 and 2400 are leap year because both conditions are true.
+	    //             But, 1700 and 1800 are also divisible by 100 but not by 400.Hence, they are not leap year.
+	    //Algo1- Check the year is divisible by 100 and 400.
+	    
+	    //2. yearly-  Check not divisibe by 100 and divisible by 4.
+	    //Algo2- Check if the year is  not divisibe by 100 and divisible by 4.
+	    if((year%100 ==0) && (year%400 ==0)|| (year%100 != 0)&& (year%4==0))
+	    {
+	        System.out.println("leap year");
+	    }
+	    else
+	    {
+	        System.out.println("not leap year");
+	        
+	    }
+	}
+}
---
+++ day4/2.java
@@ -0,0 +1,49 @@
+/*
+  Ask user to enter age, sex ( M or F ), marital status ( Y or N ) and then using following rules print their place of service.
+if employee is female, then she will work only in urban areas.
+
+if employee is a male and age is in between 20 to 40 then he may work in anywhere
+
+if employee is male and age is in between 40 t0 60 then he will work in urban areas only.
+
+And any other input of age should print "ERROR".
+*/
+
+
+import java.util.*;
+
+public class Main{
+
+	public static void main (String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+
+		System.out.println("Enter sex: M/F");
+		int sex = sc.next().charAt(0);
+		
+		
+		
+		// checking if user is female
+		if(sex == 'F') {
+			System.out.println("You will work in urban areas");
+		}
+		
+		// checking if user is male
+		if(sex == 'M') {
+		    
+		System.out.println("Enter your age");
+		int age = sc.nextInt();
+		    
+			if((age >= 20) && (age < 40)) {
+				System.out.println("You may work anywhere");
+			}
+			else if((age >= 40) && (age < 60)) {
+				System.out.println("You will work only in urban areas");
+			}
+			
+			else {
+				System.out.println("ERROR");
+			}
+		}
+	}
+}
