5aaabd062d52ab4e7b1ac8d01d16910f0d761216
Riya Chatterjee <56023007+rrriiiyyyaaa@users.noreply.github.com>
2021-11-29 17:12:48 +0530

    Update 1.java

--- day4/1.java
+++ day4/1.java
@@ -11,13 +11,13 @@ public class Main
 	    // Take integer variable year and assign a value to it.
 	    int year = sc.nextInt();
 	    
-	    // 1. century- if it is divisibe by 100 and 400 both, it is a leap year (century).  Ex- 2000 and 2400 are leap year because both conditions are true.
+	    // 1. century- if it is divisibe by and 400 , it is a leap year (century).  Ex- 2000 and 2400 are leap year because both conditions are true.
 	    //             But, 1700 and 1800 are also divisible by 100 but not by 400.Hence, they are not leap year.
 	    //Algo1- Check the year is divisible by 100 and 400.
 	    
 	    //2. yearly-  Check not divisibe by 100 and divisible by 4.
 	    //Algo2- Check if the year is  not divisibe by 100 and divisible by 4.
-	    if((year%100 ==0) && (year%400 ==0)|| (year%100 != 0)&& (year%4==0))
+	    if((year%400 ==0)|| (year%100 != 0)&& (year%4==0))
 	    {
 	        System.out.println("leap year");
 	    }
