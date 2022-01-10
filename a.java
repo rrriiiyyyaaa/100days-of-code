2b058b0141306387b14e5dbaf496aa834075be5f
Riya Chatterjee <56023007+rrriiiyyyaaa@users.noreply.github.com>
2022-01-10 18:10:37 +0530

    recursion

---
+++ a.java
@@ -0,0 +1,41 @@
+using string concatenation:
+public class Main
+{
+	public static void main(String[] args) {
+		String s = "abc";
+		String rev = "";
+		int length = s.length();             //3
+		for ( int i =length-1;i>=0;i-- )          //2 -> 1  -> 0
+		{
+		    rev = rev + s.charAt(i);               //c  -> b ->a = cba
+		}
+		System.out.println("Reverse string is: " + rev);
+	}
+}
+
+Using character array:
+public class Main
+{
+	public static void main(String[] args) {
+		String s = "abc";
+		char a[] = s.toCharArray();
+		String rev = "";
+		int length = s.length();             //3
+		for ( int i =length-1;i>=0;i-- )          //2 -> 1  -> 0
+		{
+		    rev = rev + a[i];               //c  -> b ->a = cba
+		}
+		System.out.println("Reverse string is: " + rev);
+	}
+}
+
+
+Using string buffer:
+public class Main
+{
+	public static void main(String[] args) {
+	    String str = "abc";
+	    StringBuffer sb = new StringBuffer(str);
+	    System.out.println(sb.reverse());
+	}
+}
---
+++ b.java
@@ -0,0 +1,23 @@
+public class Main
+{
+   
+	public static void main(String[] args)
+	{
+		String str = "How To Do In Java ";
+
+		String reverseString = reverseString(str);
+
+		System.out.println(reverseString);
+	}
+
+	public static String reverseString(String str)
+	{
+		if (str.isEmpty())
+		{
+		     return str;
+		}
+
+		//Calling function recursively
+		return reverseString(str.substring(1)) + str.charAt(0);
+	}
+}
